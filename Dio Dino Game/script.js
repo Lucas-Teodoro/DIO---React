aefcec41cde6e5a98a65454587ed059aed1e6003
Lucas-Teodoro <30679692+Lucas-Teodoro@users.noreply.github.com>
2021-11-07 00:02:40 -0300

    Create script.js

---
+++ Dio Dino Game/script.js
@@ -0,0 +1,31 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event){
+	if(event.keyCode === 32){
+		jump();
+	}
+}
+
+function jump(){
+	let position = 0;
+
+	let upInterval = setInterval(()=> {
+		if(position >= 150){
+			clearInterval(upInterval);
+			//Caindo
+			let downInterval = setInterval(()=>{
+				if (position <=0){
+					clearInterval(downInterval);
+				}else{
+					position -= 20;
+					dino.style.bottom = position + 'px';
+				}
+			},20);
+		}else{
+			//Pulando
+			position += 20;
+			dino.style.bottom = position + 'px';
+		}
+	}, 20);
+}
+document.addEventListener('keyup', handleKeyUp);
